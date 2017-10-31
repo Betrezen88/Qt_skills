571f80b77bf337c0e819a01724e0ec8c1860f950
miwy <michal.wyszynski@mobica.com>
2017-10-31 15:09:07 +0100

    Basic UI.

--- Module_8/ThreadWorker/Dialog.cpp
+++ Module_8/ThreadWorker/Dialog.cpp
@@ -1,11 +1,41 @@
 #include "Dialog.h"
 
+#include <QLabel>
+#include <QVBoxLayout>
+
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
 {
+    m_quitBtn = new QPushButton( "Quit" );
+    m_runBtn = new QPushButton( "Run" );
+    m_resultView = new QTextEdit();
+
+    m_resultView->setReadOnly( true );
+
+    connect( m_quitBtn, &QPushButton::clicked, this, &Dialog::close );
+    connect( m_runBtn, &QPushButton::clicked, this, &Dialog::startWorker );
+
+    QVBoxLayout *resultL = new QVBoxLayout();
+    resultL->addWidget( new QLabel("Result:") );
+    resultL->addWidget( m_resultView );
+
+    QHBoxLayout *buttonsL = new QHBoxLayout();
+    buttonsL->addWidget( m_runBtn );
+    buttonsL->addWidget( m_quitBtn );
+
+    QVBoxLayout *all = new QVBoxLayout();
+    all->addLayout( resultL );
+    all->addLayout( buttonsL );
+
+    setLayout( all );
 }
 
 Dialog::~Dialog()
+{
+
+}
+
+void Dialog::startWorker()
 {
 
 }
--- Module_8/ThreadWorker/Dialog.h
+++ Module_8/ThreadWorker/Dialog.h
@@ -2,6 +2,8 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QPushButton>
+#include <QTextEdit>
 
 class Dialog : public QDialog
 {
@@ -10,6 +12,14 @@ class Dialog : public QDialog
 public:
     Dialog(QWidget *parent = 0);
     ~Dialog();
+
+private slots:
+    void startWorker();
+
+private:
+    QPushButton *m_quitBtn;
+    QPushButton *m_runBtn;
+    QTextEdit *m_resultView;
 };
 
 #endif // DIALOG_H
