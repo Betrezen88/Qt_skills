96cf386037da695966a2a0fad555a487651da787
miwy <michal.wyszynski@mobica.com>
2017-10-31 16:55:14 +0100

    QtConcurrent::run() example.

---
+++ Module_8/Concurrent/external.h
@@ -0,0 +1,17 @@
+#ifndef EXTERNAL_H
+#define EXTERNAL_H
+
+#include <QString>
+#include <QElapsedTimer>
+
+QString function() const
+{
+    QElapsedTimer timer;
+    timer.start();
+    QString result;
+
+
+    return result;
+}
+
+#endif // EXTERNAL_H
